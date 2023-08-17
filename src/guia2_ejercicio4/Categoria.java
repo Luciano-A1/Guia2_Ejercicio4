d544c89f34895556944e860ed7f3ad1bf71fc862
Luciano-A1 <141652211+Luciano-A1@users.noreply.github.com>
2023-08-17 10:33:16 -0300

    Initial commit

---
+++ src/guia2_ejercicio4/Categoria.java
@@ -0,0 +1,9 @@
+package guia2_ejercicio4;
+
+public enum Categoria {
+    COMESTIBLE,
+    LIMPIEZA,
+    PERFUMERIA,
+    
+    
+}
---
+++ src/guia2_ejercicio4/Productos.java
@@ -0,0 +1,81 @@
+package guia2_ejercicio4;
+
+public class Productos implements Comparable<Productos> {
+
+    private int codigo;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private Categoria rubro;
+
+    public Productos(int codigo, String descripcion, double precio, int stock, Categoria rubro) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.rubro = rubro;
+    }
+
+    public Productos(Categoria rubro) {
+        this.rubro = rubro;
+    }
+    
+    
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Categoria getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(Categoria rubro) {
+        this.rubro = rubro;
+    }
+
+    @Override
+    public int compareTo(Productos t) {
+        if (codigo == t.codigo) {
+            return 0;
+        } else if (codigo > t.codigo) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Productos{" + "rubro=" + rubro + '}';
+    }
+    
+}
